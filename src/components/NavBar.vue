42e905a814ea2e16fa0e67295f67d3df81ee8b42
Gabriel Angelo <gabriel.angelsad1@gmail.com>
2020-08-01 18:10:07 -0300

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="navbar">
+        lil
+
+        <nav>
+          <ul>
+            
+          </ul>
+        </nav>
+        
+
+
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+    header.navbar {
+        min-width: 100vw;
+        height: 180px;
+        border: 1px solid red;
+    }
+
+
+</style>
